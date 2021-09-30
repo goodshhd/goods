482aa6fcde1189650e83b30e3ab1fc1a25640ec4
Robert Kulik <54913283+RoboRobo12@users.noreply.github.com>
2021-09-30 17:23:37 +0300

    Feature/sign in rebuild (#1)
    * initial commit to workboard
    
    * implement new view of sign in page

--- pages/signIn/index.jsx
+++ pages/signIn/index.jsx
@@ -1,32 +1,49 @@
 import React from "react";
 import { signIn, getSession } from "next-auth/client";
 import Image from "next/image";
-import logo from '/Users/robertkulik/My-Projects/goods/public/static/images/logo-regular.png';
+import withLayout from "../../components/withLayout";
+import arrowDown from '../../public/icon/arrow-down.svg';
+import logoWhite from '../../public/images/logo-goods-white.svg';
+import Button from "../../components/Button";
 
 
 const SignIn = () => {
     return (
-        <div className="min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-            <div className="max-w-md w-full space-y-8">
-                <div>
-                    <div className="flex w-full items-center justify-center" >
-                        <Image src={logo} height={100} width={100} alt='leaf-logo' />
+        <div className='flex'>
+            <div style={{background: "url('../../images/sign-in-bg.png')"}} className='hidden w-1/2 bg-no-repeat bg-cover min-h-screen md:flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8'>
+                <div className='flex flex-col justify-center items-center'>
+                    <Image src={logoWhite}/>
+                    <div className='mt-8'>
+                        <h2 className="text-center text-6xl font-extrabold text-white font-bold">
+                            Welcome to
+                        </h2>
+                        <h2 className="text-center text-6xl font-extrabold text-white font-bold">
+                            Goods
+                        </h2>
                     </div>
-                    <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-                        Sign in to your account
-                    </h2>
+                    <span className='text-white font-normal text-md mt-4 w-96 text-center'>
+                        the place where you can organize your productivity and managing data from business
+                    </span>
                 </div>
-                <div>
-                    <button onClick={signIn} className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-yellow-500 hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500">
-                        Sign in
-                    </button>
+            </div>
+            <div className="min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8 w-full md:w-1/2">
+                <div className="max-w-md w-full space-y-8 items-center justify-center flex flex-col">
+                    <div className='flex flex-col items-center justify-center'>
+                        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900 w-48">
+                            Sign in to your account
+                        </h2>
+                        <Image src={arrowDown} />
+                    </div>
+                    <div className='w-48'>
+                        <Button onClick={signIn} buttonText='Sign in' />
+                    </div>
                 </div>
             </div>
         </div>
     )
 }
 
-export default SignIn;
+export default withLayout(SignIn);
 
 export async function getServerSideProps(context) {
     const session = await getSession(context);
