2b831619826e2129d5cdbcce8461353f77870447
Robert Kulik <54913283+RoboRobo12@users.noreply.github.com>
2021-10-01 09:59:22 +0300

    Feature/workboard (#2)
    * initial commit to workboard
    
    * separated withLayout HOC a bit changed structure
    
    * Implement initial set up of workboard

--- components/withLayout/index.jsx
+++ components/withLayout/index.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import Head from "next/head";
+import Header from "../Header";
 
-const withLayout = Component => () => {
+const withLayoutAuth = Component => () => {
     return (
         <>
             <Head>
                 <title>Leaf App</title>
                 <link rel='icon' href='/favicon.ico'/>
             </Head>
+            <Header />
             <Component />
         </>
     );
 };
 
-export default withLayout;
+export default withLayoutAuth;
--- pages/signIn/index.jsx
+++ pages/signIn/index.jsx
@@ -1,49 +1,55 @@
 import React from "react";
 import { signIn, getSession } from "next-auth/client";
 import Image from "next/image";
-import withLayout from "../../components/withLayout";
 import arrowDown from '../../public/icon/arrow-down.svg';
 import logoWhite from '../../public/images/logo-goods-white.svg';
 import Button from "../../components/Button";
+import Head from "next/head";
 
 
 const SignIn = () => {
     return (
-        <div className='flex'>
-            <div style={{background: "url('../../images/sign-in-bg.png')"}} className='hidden w-1/2 bg-no-repeat bg-cover min-h-screen md:flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8'>
-                <div className='flex flex-col justify-center items-center'>
-                    <Image src={logoWhite}/>
-                    <div className='mt-8'>
-                        <h2 className="text-center text-6xl font-extrabold text-white font-bold">
-                            Welcome to
-                        </h2>
-                        <h2 className="text-center text-6xl font-extrabold text-white font-bold">
-                            Goods
-                        </h2>
-                    </div>
-                    <span className='text-white font-normal text-md mt-4 w-96 text-center'>
+        <>
+            <Head>
+                <title>Leaf App</title>
+                <link rel='icon' href='/favicon.ico'/>
+            </Head>
+            <div className='flex'>
+                <div style={{background: "url('../../images/sign-in-bg.png')"}} className='hidden w-1/2 bg-no-repeat bg-cover min-h-screen md:flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8'>
+                    <div className='flex flex-col justify-center items-center'>
+                        <Image src={logoWhite}/>
+                        <div className='mt-8'>
+                            <h2 className="text-center text-6xl font-extrabold text-white font-bold">
+                                Welcome to
+                            </h2>
+                            <h2 className="text-center text-6xl font-extrabold text-white font-bold">
+                                Goods
+                            </h2>
+                        </div>
+                        <span className='text-white font-normal text-md mt-4 w-96 text-center'>
                         the place where you can organize your productivity and managing data from business
                     </span>
-                </div>
-            </div>
-            <div className="min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8 w-full md:w-1/2">
-                <div className="max-w-md w-full space-y-8 items-center justify-center flex flex-col">
-                    <div className='flex flex-col items-center justify-center'>
-                        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900 w-48">
-                            Sign in to your account
-                        </h2>
-                        <Image src={arrowDown} />
-                    </div>
-                    <div className='w-48'>
-                        <Button onClick={signIn} buttonText='Sign in' />
                     </div>
                 </div>
+                <div className="min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8 w-full md:w-1/2">
+                    <div className="max-w-md w-full space-y-8 items-center justify-center flex flex-col">
+                        <div className='flex flex-col items-center justify-center'>
+                            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900 w-48">
+                                Sign in to your account
+                            </h2>
+                            <Image src={arrowDown} />
+                        </div>
+                        <div className='w-48'>
+                            <Button onClick={signIn} buttonText='Sign in' />
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 
-export default withLayout(SignIn);
+export default SignIn;
 
 export async function getServerSideProps(context) {
     const session = await getSession(context);
