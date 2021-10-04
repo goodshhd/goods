a237c9b2c5900cfe12007da28521773a41aa2685
robertkulik <idsdesign9796@gmail.com>
2021-10-04 11:23:06 +0300

    implement filter component (without Func )

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -4,20 +4,21 @@ import logo from '../../public/images/logo-goods-white.svg';
 import DropdownWrapper from '../DropdownWrapper';
 import {signOut, useSession} from 'next-auth/client';
 import Nav from '../Nav';
-import SettingButton from '../SettingButton';
+import SmallButton from '../SmallButton';
 import MobileMenuButton from '../MobileMenuButton';
 import MobileMenu from '../MobileMenu';
 import useOutsideClick from "../../utils/hooks/useOutsideClick";
+import settingIcon from '../../public/icon/setting-icon.svg';
 
 const Header = () => {
     const [session] = useSession();
     const [showDD, setShowDD] = useState(false);
     const [toggleMobileMenu, setToggleMobileMenu] = useState(false);
     const handleCloseDD = () => setShowDD(false);
-    const handleShowDD = () => setShowDD(true);
+    const handleShowDD = () => setShowDD(!showDD);
 
     const wrapper = useRef(null);
-    useOutsideClick(wrapper, () => handleCloseDD())
+    useOutsideClick(wrapper, handleCloseDD);
 
     const tabs = [
         {
@@ -75,7 +76,7 @@ const Header = () => {
                     <div className='hidden md:block'>
                         <div className='ml-4 flex items-center md:ml-6'>
                             <div className='ml-3 relative' ref={wrapper}>
-                                <SettingButton handleShowDropDown={handleShowDD}/>
+                                <SmallButton handleClick={handleShowDD} icon={settingIcon} color='bg-yellow-600'/>
                                 <DropdownWrapper toggle={showDD}>
                                     <div
                                         className='origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none'>
@@ -83,7 +84,8 @@ const Header = () => {
 
                                         <a href='#' className='block px-4 py-2 text-sm text-gray-700'>Settings</a>
 
-                                        <a href='#' className='block px-4 py-2 text-sm text-gray-700' onClick={signOut}>Sign out</a>
+                                        <a href='#' className='block px-4 py-2 text-sm text-gray-700' onClick={signOut}>Sign
+                                            out</a>
                                     </div>
                                 </DropdownWrapper>
                             </div>
@@ -97,7 +99,7 @@ const Header = () => {
             {
                 toggleMobileMenu &&
                 <div className='md:hidden'>
-                    <MobileMenu user={session.user || 'John Doe'} menuTabs={tabs} settingsTabs={mobileSettingsTabs} />
+                    <MobileMenu user={session.user || 'John Doe'} menuTabs={tabs} settingsTabs={mobileSettingsTabs}/>
                 </div>
             }
         </nav>
---
+++ components/SmallButton/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Image from 'next/image';
+import PropTypes from 'prop-types';
+
+const SmallButton = ({handleClick, icon, color}) => (
+    <div>
+        <button type='button'
+                className={`p-2 max-w-xs ${color} rounded-md flex items-center text-sm`}
+                 onClick={handleClick}>
+            <span className='sr-only'>Open user menu</span>
+            <Image className='h-8 w-8'
+                   src={icon}
+                   alt='setting'/>
+        </button>
+    </div>
+);
+
+export default SmallButton;
+
+SmallButton.propTypes = {
+    handleShowDropDown: PropTypes.func,
+    icon: PropTypes.object
+};
+
+SmallButton.defaultProps = {
+    handleClick: () => {},
+    icon: {}
+}
