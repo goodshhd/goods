32498b44e7a5be70d4d65ea521084463a0127ea5
robertkulik <idsdesign9796@gmail.com>
2021-10-06 13:44:55 +0300

    implement Filter logic

--- components/Input/index.jsx
+++ components/Input/index.jsx
@@ -2,12 +2,18 @@ import React from 'react';
 
 import PropTypes from 'prop-types';
 
-const Input = ({placeholder, label}) => {
+const Input = ({placeholder, label, onChange, value, type}) => {
     return (
         <div>
             <label className='text-sm font-medium text-gray-700'>{label}</label>
-            <div className='mt-1 relative rounded-md shadow-sm' >
-                <input type='text' className='focus:ring-yellow-500 placeholder-gray-300 focus:border-yellow-500 w-full pl-4 pr-12 sm:text-sm border-gray-300 rounded-md' placeholder={placeholder || 'Default Placeholder'} />
+            <div className='mt-1 relative rounded-md shadow-sm'>
+                <input
+                    type={type}
+                    value={value ? value : ''}
+                    onChange={onChange}
+                    placeholder={placeholder || 'Default Placeholder'}
+                    className='focus:ring-yellow-500 placeholder-gray-300 focus:border-yellow-500 w-full pl-4 pr-12 sm:text-sm border-gray-300 rounded-md'
+                />
             </div>
         </div>
     );
@@ -16,13 +22,19 @@ const Input = ({placeholder, label}) => {
 export default Input;
 
 Input.propTypes = {
+    value: PropTypes.any,
+    type: PropTypes.string,
     label: PropTypes.string,
+    onChange: PropTypes.func,
     placeholder: PropTypes.string
 };
 
 Input.defaultProps = {
+    type: '',
+    value: '',
     label: '',
-    placeholder: ''
+    placeholder: '',
+    onChange: () => {}
 };
 
 
--- recoil/atoms.js
+++ recoil/atoms.js
@@ -2,9 +2,78 @@ import { atom } from 'recoil';
 import { signOut } from "next-auth/client";
 
 
-export const headerTabsState = atom({
-    key: 'headerTabsState',
-    default: []
+export const userData = atom({
+    key: 'userData',
+    default: {}
+});
+
+export const tableData = atom({
+    key: 'tableData',
+    default: [
+        {
+            userId: 1,
+            code: 'VB720',
+            company: 'ART-UA',
+            id: 1,
+            title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+            body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+        },
+        {
+            userId: 1,
+            code: 'VB800',
+            company: 'A-UA',
+            id: 1,
+            title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+            body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+        },
+        {
+            userId: 1,
+            code: 'VB300',
+            company: 'A-UA',
+            id: 1,
+            title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+            body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+        },
+        {
+            userId: 1,
+            code: 'KB800',
+            company: 'A-UA',
+            id: 1,
+            title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+            body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+        },
+        {
+            userId: 1,
+            code: 'SB80',
+            company: 'FA-UA',
+            id: 1,
+            title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+            body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+        },
+        {
+            userId: 1,
+            code: 'EB00',
+            company: 'RRR-UA',
+            id: 1,
+            title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+            body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+        },
+    ]
+});
+
+export const selectedFilter = atom({
+    key: 'selectedFilter',
+    default: ''
+});
+
+export const codeInputState = atom({
+    key: 'codeInputState',
+    default: ''
+});
+
+export const companyDataState = atom({
+    key: 'companyDataState',
+    default: ''
 });
 
 export const mobileTabsState = atom({
@@ -28,7 +97,7 @@ export const mobileTabsState = atom({
     ]
 });
 
-export const userData = atom({
-    key: 'userData',
-    default: {}
-})
+export const headerTabsState = atom({
+    key: 'headerTabsState',
+    default: []
+});
