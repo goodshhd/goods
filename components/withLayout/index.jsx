7fd3c08f63010bd1a6642480514b36f927d8e04a
Robert Kulik <54913283+RoboRobo12@users.noreply.github.com>
2021-10-06 10:54:21 +0300

    Bug/fix navbar issue (#10)
    * fix nav bar issue
    
    * additional fix

--- components/withLayout/index.jsx
+++ components/withLayout/index.jsx
@@ -7,12 +7,43 @@ import {useRecoilState} from 'recoil';
 import {useSession} from 'next-auth/client';
 
 import PropTypes from 'prop-types';
-import {userData} from '../../recoil/atoms';
+import {headerTabsState, userData} from '../../recoil/atoms';
 
 const withLayout = Component => () => {
 
     const session = useSession();
-    const [user,setUser] = useRecoilState(userData);
+    const [,setUser] = useRecoilState(userData);
+    const [,setSessionData] = useRecoilState(headerTabsState);
+
+    const userEmail = session[0].user.email;
+
+    useEffect(() => {
+        setSessionData((oldData) =>
+            [
+                ...oldData,
+                {
+                    link: `/${userEmail}/workboard`,
+                    title: 'Workboard'
+                },
+                {
+                    link: '#',
+                    title: 'Team'
+                },
+                {
+                    link: '#',
+                    title: 'Projects'
+                },
+                {
+                    link: '#',
+                    title: 'Calendar'
+                },
+                {
+                    link: '#',
+                    title: 'Reports'
+                },
+            ]
+        );
+    }, [userEmail]);
 
     useEffect(() => {
         setUser(session[0].user);
