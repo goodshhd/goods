52107a38173da38aa30d137fa13b50dfb3404da0
robertkulik <idsdesign9796@gmail.com>
2021-11-13 23:54:56 +0200

    added useHttp hook

---
+++ utils/hooks/useHttp/index.js
@@ -0,0 +1,33 @@
+import {useState} from "react";
+
+
+export const useHttp = () => {
+    const[loading, setLoading] = useState(false);
+    const[error, setError] = useState(null);
+
+    const request = async (url, method ='GET', data = null,) => {
+        setLoading(true);
+        const response = await fetch(url, {
+            method,
+            body: method === 'POST' ? JSON.stringify(data): null ,
+            headers: {
+                'Content-type': "application/json"
+            }
+        })
+            .then(res => res.json())
+        if(!response) {
+            throw new Error(response?.message || 'Error');
+        }
+
+        setLoading(false);
+
+        return response
+
+    };
+
+    const clearError = () => setError(null);
+
+    return{loading, error, request, clearError}
+}
+
+export default useHttp;
